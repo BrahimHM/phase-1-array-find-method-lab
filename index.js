a9901953391753f6e3c164dcd1106e60d9b28ac9
Brahim HM <brahimhm98@gmail.com>
2023-08-22 15:32:44 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+const superbowlWin=record=>{
+    const found= record.find((element) => element.result==="W")
+   if (found ) {return found.year}
+   else {return undefined}
+}
